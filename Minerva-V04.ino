6c0cfc7175c87b68a1002ee053a37b76bd13146c
Ahtoous <100285947+athosaraujo@users.noreply.github.com>
2023-05-29 15:09:37 -0300

    Updated to V04

--- Minerva-V04.ino
+++ Minerva-V04.ino
@@ -1,4 +1,5 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 #include "ESP32_Servo.h"
 
 #include <BLEDevice.h>
@@ -12,7 +13,6 @@ Servo myShovel;
 int pos = 0;
 bool openH = false;
 bool shovelDown = false;
-
 
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
@@ -49,17 +49,35 @@ class MyCallbacks : public BLECharacteristicCallbacks {
   }
 };
 
-Stepper myStepper(2048, 18, 22, 19, 23);
-// (passosPorRevolução, Ini1, Ini3, Ini2, Ini4);
+long positions[4] = {100,100,100,100};
+
+AccelStepper L1(AccelStepper::FULL4WIRE, 33, 32, 25, 5);
+AccelStepper L2(AccelStepper::FULL4WIRE, 13, 12, 14, 27);
+AccelStepper R1(AccelStepper::FULL4WIRE, 23, 22, 21, 19);
+AccelStepper R2(AccelStepper::FULL4WIRE, 17, 16, 4, 2);
+
+// (Ini1, Ini2, Ini3, Ini4);
+
+MultiStepper motores;
 
 void setup() {
   Serial.begin(115200);
-  myStepper.setSpeed(13);  // Velocidade de rotação dos motores de passo
-  myHatch.attach(4);
-  myShovel.attach(2);
+  myHatch.attach(26);
+  myHatch.write(90);
+  myShovel.attach(15);
+  myShovel.write(135);
+
+
+  L1.setMaxSpeed(10);L2.setMaxSpeed(10);
+  R1.setMaxSpeed(10);R2.setMaxSpeed(10);
+
+  motores.addStepper(L1);
+  motores.addStepper(L2);
+  motores.addStepper(R1);
+  motores.addStepper(R2);
 
   // Configuração e inicialização do serviço Bluetooth Low Energy
-  BLEDevice::init("J4S0N-V1");  // Nome do Protótipo
+  BLEDevice::init("Ringo-V4");  // Nome do Protótipo
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -93,50 +111,63 @@ void convertControlpad() {
     while (rxString == "!B11:") {
       if (openH == false) {
         openH = true;
-        for (int i = 0; i <= 180; i++) {
+        for (int i = 90; i <= 150; i++) {
           myHatch.write(i);
         }
-      }
-    }
-    if (openH == true) {
-      openH = false;
-      for (int i = 180; i >= 0; i--) {
-        myHatch.write(i);
       }
     }
   }  // Apertou Botão 01
 
   else if (rxString == "!B10;") {Serial.println("Largou botão 01");} 
 
+  else if (rxString == "!B318") {
+  Serial.println("Apertou botão 03");
+  while(rxString == "!B318"){
+  if (openH == true) {
+      openH = false;
+      for (int i = 180; i >= 90; i--) {
+        myHatch.write(i);
+      }
+    }}
+    } 
+  else if (rxString == "!B309") {Serial.println("Largou botão 03");}
+
+
   else if (rxString == "!B219"){
+    Serial.println("Apertou botão 02");
     while (rxString == "!B219") {
       if (shovelDown == false) {
         shovelDown = true;
-        for (int i = 0; i >= -90; i--) {
+        for (int i = 135; i >= 0; i--) {
           myShovel.write(i);
         }
-      }
-    }
-    if (shovelDown == true) {
-      shovelDown = false;
-      for (int i = -90; i <= 0; i++) {
-        myShovel.write(i);
       }
     }
   }  // Apertou Botão 02
   
   else if (rxString == "!B20:") {Serial.println("Largou botão 02");}
 
-  else if (rxString == "!B318") {Serial.println("Apertou botão 03");} 
-  else if (rxString == "!B309") {Serial.println("Largou botão 03");}
-
-  else if (rxString == "!B417") {Serial.println("Apertou botão 04");} 
+  else if (rxString == "!B417") {
+    Serial.println("Apertou botão 04");
+      if (shovelDown == true) {
+      shovelDown = false;
+      for (int i = 0; i <= 135; i++) {
+        myShovel.write(i);
+      }
+    }
+    } 
   else if (rxString == "!B408") {Serial.println("Largou botão 04");}
 
   else if (rxString == "!B516") {  // Seta do controle apontando para cima
     Serial.println("Going foward");
     while (rxString == "!B516") {
-      myStepper.step(10);
+      motores.moveTo(positions);
+      motores.runSpeedToPosition();
+      delay(1000);
+      L1.setCurrentPosition(0);
+      L2.setCurrentPosition(0);
+      R1.setCurrentPosition(0);
+      R2.setCurrentPosition(0);
       }
     } 
   else if (rxString == "!B507") Serial.println("Largou seta cima");
@@ -144,7 +175,7 @@ void convertControlpad() {
   else if (rxString == "!B615") {  // Seta do controle apontando para baixo
     Serial.println("Going Backwards");
     while (rxString == "!B615") {
-      myStepper.step(10);
+      motores.run();
       }
     } 
   else if (rxString == "!B606") {Serial.println("Largou seta baixo");}
